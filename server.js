efda24a54b589bc262ffe9ee367326ea1c05cbbe
huynguyen321 <huy.nguyen22@student.passerellesnumeriques.org>
2021-05-13 18:49:21 +0700

    upload image

--- server.js
+++ server.js
@@ -1,29 +1,69 @@
+const fs = require("fs");
+const jsonServer = require("json-server");
 
-const host = "https://demo-json-server123.herokuapp.com/";
-const express = require("express");
-const server = express();
-const fileupload = require("express-fileupload");
-server.use(fileupload());
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const host = __dirname;
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
 
-server.get('/',(req,res,next)=>{
-  res.status(200).send("OK")
-})
-
-server.post("/upload", function (req, res, next) {
-  const file = req.files.photo;
-  const filename = (new Date()).getTime()
-  file.mv(`./uploads/ ${filename}.${(file.name).split('.')[1]}`, function (err, result) {
-    if (err) throw err;
-    res.send({
-      success: true,
-      message: "File uploaded!",
-      url : `${__dirname}/uploads/ ${filename}.${(file.name).split('.')[1]}`
-    });
-  });
+server.get("/api", (req, res) => {
+  res.send("Welcome to API of Totoro Milk Tea shop");
 });
 
-const PORT = process.env.PORT || 3000;
+// 'http://totoromilkteaapi.herokuapp.com/';
 
+// put ads
+server.use((req, res, next) => {
+  if (req.method === "POST" && req.path === "/api/upload") {
+    const date = new Date();
+    let updateAt = date.getTime();
+
+    const data = req.body;
+    const image = data["image"];
+
+    // process to a image file if it is a new image
+
+    if (image.indexOf(".png") < 0 && image.indexOf(".jpg") < 0) {
+      const fileType = image
+        .match(/[^:]\w+\/[\w-+\d.]+(?=;|,)/)[0]
+        .split("/")[1];
+      var endFile = "";
+      if (fileType == "jpeg") {
+        endFile = "jpg";
+      } else if (fileType == "png") {
+        endFile = "png";
+      }
+
+      const filename = date.getTime() + "." + endFile;
+      // create new image ads
+      fs.writeFile(
+        `image_ads/${filename}`,
+        image.split(",")[1],
+        "base64",
+        function (err) {
+          console.log(err);
+        }
+      );
+
+      data.image = `${host}/image_ads/${filename}`;
+      req.body = data;
+    }
+
+    res.json(req.body);
+  }
+
+  // Continue to JSON Server router
+  next();
+});
+
+// set port
+const PORT = process.env.PORT || 3000;
+// Use default router
+server.use("/api", router);
+// Start server
 server.listen(PORT, () => {
-  console.log("Demo JSON Server is running");
+  console.log("TotoroMilkTea website's JSON Server is running");
 });
